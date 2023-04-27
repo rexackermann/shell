adca342a84fa8e655564f4182ca51cac42478072
Rex Ackermann <ackermann88888@gmail.com>
2023-04-28 00:11:20 +0600

    setup_update

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 cd ~/
 
-mv .oh-my-zsh .oh-my-zsh.$(date +%s)
+mv ~/.oh-my-zsh ~/.oh-my-zsh.$(date +%s)
 
 curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | bash  # installs oh-my-zsh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
