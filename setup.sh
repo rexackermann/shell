84d7bc4b77a443a33d770766487838b5330d4cbb
Rex Ackermann <ackermann88888@gmail.com>
2023-05-16 00:19:27 +0600

    update_1684174767

--- setup.sh
+++ setup.sh
@@ -70,7 +70,7 @@ termuxexec() {
      if [[ $(uname -a | awk '{print $14}') == "Android" ]]; then
           echo -e "termux detected"
           # echo "/data/data/com.termux/files/usr/bin/sshd -p 43434" >> ~/.zshrc
-          sed 's/my_cpu_temp/\#\ my_cpu_temp/' ~/shell/.p10k.zsh > ~/shell/.p10k.tmp
+          sed 's/my_cpu_temp\ \ /\#\ my_cpu_temp\ \ /' ~/shell/.p10k.zsh > ~/shell/.p10k.tmp
           mv ~/shell/.p10k.tmp ~/shell/.p10k.zsh
           # echo "sed 's/my_cpu_temp/\#\ my_cpu_temp/' .p10k.zsh > .p10k.zsh"
      fi
